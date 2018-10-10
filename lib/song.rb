892ee7e19f4c98697b1d3170209844d1e8ed36c5
Michael Kim <m.kim4247@gmail.com>
2018-10-10 23:42:14 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,54 @@
+require 'pry'
+
+class Song  
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  @@genre_count = {}
+  @@artist_count = {}
+  
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre 
+    
+    
+    @@count += 1 
+    @@genres << genre 
+    @@artists << artist
+    
+  end 
+  
+  def self.count 
+    @@count
+  end 
+  
+  def self.genres 
+    @@genres.uniq 
+  end 
+  
+  def self.artists 
+    @@artists.uniq
+  end 
+  
+  def self.genre_count 
+    count = Hash.new(0)
+    @@genres.each do |genre|
+      count[genre] += 1 
+    end 
+    count 
+  end 
+  
+  def self.artist_count
+    count = Hash.new(0)
+    @@artists.each do |artist|
+      count[artist] += 1 
+    end 
+    count 
+  end 
+  
+end 
+
+
